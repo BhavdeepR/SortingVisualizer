a52e95a8ae633ad951f256802b69c6ce8b76f4fc
BhavdeepR <bhavdeepr19@gmail.com>
2023-06-06 16:52:12 -0700

    Add Insertion Sort

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,49 +1,161 @@
 import React from 'react';
+import {doInsertionSort, getMergeSortAnimations} from '../sortingAlgorithms/sortingAlgorithms.js';
 import './SortingVisualizer.css';
 
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 200;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = 'turquoise';
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = 'red';
+
 export default class SortingVisualizer extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            array: [],
-        };
+    this.state = {
+      array: [],
+    };
+  }
+
+  componentDidMount() {
+    this.resetArray();
+  }
+
+  resetArray() {
+    const array = [];
+    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+      array.push(randomIntFromInterval(5, 730));
     }
+    this.setState({array});
+  }
 
-    componentDidMount() {
-        this.resetArray();
+  mergeSort() {
+    const animations = getMergeSortAnimations(this.state.array);
+    // iterate through earch of the two element arrays in animations array
+    for (let i = 0; i < animations.length; i++) {
+      // these are the array bars that are being displayed
+      const arrayBars = document.getElementsByClassName('array-bar');
+      // for each comparison, three arrays are push into animations array, the first two are for
+      // color changes, the third is two update the index
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        //change the color of the array bars to show comparison and then change them back
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+        //change the color and wait the given time to change back
+      } else {
+        //this code is to change the height of the next array bar with the given height
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * ANIMATION_SPEED_MS);
+      }
     }
+  }
 
-    resetArray() {
-        const array = [];
-        for (let i = 0; i < 310; i++) {
-            array.push(randomIntFromInterval(5, 730));
-        }
-        this.setState({array});
+  quickSort() {
+  
+  }
+
+  insertionSort() {
+    const animations = doInsertionSort(this.state.array);
+    // iterate through earch of the two element arrays in animations array
+    for (let i = 0; i < animations.length; i++) {
+      // these are the array bars that are being displayed
+      const arrayBars = document.getElementsByClassName('array-bar');
+      // for each comparison, three arrays are push into animations array, the first two are for
+      // color changes, the third is two update the index
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        //change the color of the array bars to show comparison and then change them back
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+        //change the color and wait the given time to change back
+      } else {
+        //this code is to change the height of the next array bar with the given height
+        setTimeout(() => {
+          const [barOneIdx, barOneHeight, barTwoIdx, barTwoHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          const barTwoStyle = arrayBars[barTwoIdx].style;
+          barOneStyle.height = `${barOneHeight}px`;
+          barTwoStyle.height = `${barTwoHeight}px`;
+
+        }, i * ANIMATION_SPEED_MS);
+      }
     }
+  }
 
-    render() {
-        const {array} = this.state;
 
-        return (
-            <>
-            <div className='array-container'>
-                {array.map((value, idx) => (
-                    <div 
-                    className = 'array-bar' 
-                    key = {idx}
-                    style = {{height: `${value}px` }}
-                    >
-                    </div>
-                ))}
-                 <button onClick={() => this.resetArray()}>Generate New Array</button>
-            </div>
-            </>
-        )
-        
+  // NOTE: This method will only work if your sorting algorithms actually return
+  // the sorted arrays; if they return the animations (as they currently do), then
+  // this method will be broken.
+  testSortingAlgorithms() {
+    for (let i = 0; i < 100; i++) {
+      const array = [];
+      const length = randomIntFromInterval(1, 1000);
+      for (let i = 0; i < length; i++) {
+        array.push(randomIntFromInterval(-1000, 1000));
+      }
+      const javaScriptSortedArray = array.slice().sort((a, b) => a - b);
+      const mergeSortedArray = getMergeSortAnimations(array.slice());
+      console.log(arraysAreEqual(javaScriptSortedArray, mergeSortedArray));
     }
+  }
+
+  render() {
+    const {array} = this.state;
+
+    return (
+      <div className="array-container">
+        {array.map((value, idx) => (
+          <div
+            className="array-bar"
+            key={idx}
+            style={{
+              backgroundColor: PRIMARY_COLOR,
+              height: `${value}px`,
+            }}></div>
+        ))}
+        <button onClick={() => this.resetArray()}>Generate New Array</button>
+        <button onClick={() => this.mergeSort()}>Merge Sort</button>
+        <button onClick={() => this.quickSort()}>Quick Sort</button>
+        <button onClick={() => this.insertionSort()}>Insertion Sort</button>
+      </div>
+    );
+  }
 }
 
+// From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
 function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function arraysAreEqual(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) return false;
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
 }
